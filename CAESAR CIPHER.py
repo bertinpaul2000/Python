5c4fa37ec00cea6c8196fb01023397aece53d1de
bertinpaul2000 <80140490+bertinpaul2000@users.noreply.github.com>
2025-08-01 21:01:56 +0800

    CAESAR CIPHER.py

--- CAESAR CIPHER.py
+++ CAESAR CIPHER.py
@@ -1,34 +1,5 @@
 import string
 
-# user_input = []
-# machine = input("Do you want to (e)ncrypt or (d)ecrypt?\n> ")
-# shift = input("Please enter the key (0 to 25) to use.\n> ")
-# alphabets = string.ascii_uppercase
-# user_message = input("Enter the message to encrypt.\n> ")
-#
-# def encrypt_machine():
-#     encrypted_msg = ""
-#     if machine.lower() == "e":
-#         for item in user_message.upper():
-#             if item in alphabets:
-#                 index = alphabets.find(item)
-#                 new_index = index + int(shift)
-#                 if new_index > 25:
-#                     new_index -= 26
-#                 encrypted_msg += alphabets[new_index]
-#
-#     elif machine.lower() == "d":
-#         for item in user_message.upper():
-#             if item in alphabets:
-#                 index = alphabets.find(item)
-#                 new_index = index - int(shift)
-#                 if new_index < 0:
-#                     new_index += 26
-#                 encrypted_msg += alphabets[new_index]
-#     print(encrypted_msg)
-#
-# encrypt_machine()
-
 def find_machine():
     machine = ""
     while machine not in ["e", "d"]:
